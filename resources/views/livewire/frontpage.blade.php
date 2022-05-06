7f82c44f81b4051620e97c191bde6786e8dd0804
edisyst <63509871+edisyst@users.noreply.github.com>
2022-05-06 14:47:59 +0200

    Update frontpage.blade.php

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -1,4 +1,22 @@
-<div>
-    <h1>{{ $title }}</h1>
-    {!! $content !!}
-</div>
+    <div>
+      <nav class="bg-gray-900 px-3 py-2 flex items-center">
+        <div class="bg-red-500">
+            Mobile View Button 5min
+        </div>
+        <div class="bg-green-500">Logo Image</div>
+        <div class="bg-blue-500">Top Nav Links</div>
+      </nav>
+      <div>
+        <aside>
+
+        </aside>
+      </div>
+      <main>
+        <section>
+          <h1>{{ $title }}</h1>
+          <article>
+            {!! $content !!}
+          </article>
+        </section>
+      </main>
+    </div>
